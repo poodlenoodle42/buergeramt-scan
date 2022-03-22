454821a55b400ba1d028ec3924e00ac752d96ed6
Calvin Katt <katt.calvin@gmail.com>
2022-03-22 08:51:35 +0100

    Added notification via discord webhook

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # insert url which lays under "Termin berlinweit suchen" - Right click to copy
 url = "enter-url-here"
 # Interval to reload the data
-interval_time = 2
+interval_time = 300
 
+webhook_url = "webhook-url"
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 import time
-from config import url, interval_time
+from config import url, interval_time,webhook_url
+import requests
+from discord import Webhook, RequestsWebhookAdapter
+
+webhook = Webhook.from_url(webhook_url, adapter=RequestsWebhookAdapter())
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
@@ -11,18 +15,19 @@ logging.basicConfig(
 
 
 def handle_found_appointments(num_found):
-    # you can add your own handler (e.g. sounds, telegram) here
-    print("Yeah! %d possible appointments" % num_found)
-    # This Character plays the default error sound on Ubuntu. I don't know if it works for Windows though
-    print("\a")
+    #Sends information about new appointment to discord
+    webhook.send("Yeah! %d possible appointments" % num_found)
 
 def crawl():
     html = requests.get(url)
     if(html.status_code != 200):
         print("Website Error")
+        webhook.send("Website Error, Status: %d" % html.status_code)
         return
     soup = BeautifulSoup(html.text, 'html.parser')
+
     logging.info("Occupied: %d" % len(soup.find_all("td", { "class": "nichtbuchbar" })))
+
     found = len(soup.find_all("td", { "class": "buchbar" }))
     if found > 0:
         logging.info("FOUND %d POSSIBLE APPOINTMENTS!" % found)
